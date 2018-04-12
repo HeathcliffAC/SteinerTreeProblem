7de0ea42d873aaf472311057ac155fda3012936d
Emmanuel <emmanuel.romero@cimat.mx>
2018-04-12 16:04:10 -0500

    Clase SteinerTree

---
+++ SteinerTree.h
@@ -0,0 +1,34 @@
+class SteinerTreeProblem{
+	public:
+		SteinerTreeProblem(const string &fileName);
+		SteinerTreeProblem(){
+		
+		}
+		~SteinerTreeProblem(){
+		
+		}
+	private:
+		vector<vector<int> > adj;
+		vector<pair<int, int> > edges;
+};
+
+class SteinerTree{
+	public:
+		SteinerTree(){
+		
+		{
+		~SteinerTree(){
+		
+		}
+		void restart();
+		double getDistance(SteinerTree &ind);
+		friend ostream& operator<< (ostream &os, const SteinerTree &ST);
+		void print(ostream &os) const;
+		void dependentMutation(double pm);
+		void localSearch();
+		void dependentCrossover(SteinerTree *ind);
+		void calculateFitness();
+		vector<bool> I;
+		long long fitness;
+		SteinerTreeProblem *SteinerTreeproblem;
+};
